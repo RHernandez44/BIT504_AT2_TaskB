fbb3f4ad4beeb84790b3b8306ebac83bd513d605
RHernandez44 <razhanrahan@gmail.com>
2021-12-08 16:23:47 +1300

    Added paint component and update()

--- BIT504_AS2_5029791/src/PongPanel.java
+++ BIT504_AS2_5029791/src/PongPanel.java
@@ -1,19 +1,25 @@
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import javax.swing.Timer;
 import javax.swing.JPanel;
-import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	
 	// Constants
 	private final static Color BACKGROUND_COLOUR = Color.BLACK;
-
+	private final static int TIMER_DELAY = 5;
+	
 	public PongPanel() {
 		
 		// Constructor
 		setBackground(BACKGROUND_COLOUR);
+		Timer timer = new Timer(TIMER_DELAY, this);
+        timer.start();	// Creates timer loop to call actionPerformed()
 		
 	}
 	// Key press Methods
@@ -38,9 +44,18 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		// TODO Auto-generated method stub
-		
+		update();	
 	}
 	
+	private void update() {
+
+	}
 	
+	@Override
+	 public void paintComponent(Graphics g) { // Draws rectangle
+	     super.paintComponent(g);
+	     g.setColor(Color.WHITE);
+	     g.fillRect(20, 20, 100, 100);
+	 }
 	
 }
