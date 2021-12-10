6aaca1e780c916632e25f57b180ce0a3dcca13ee
RHernandez44 <razhanrahan@gmail.com>
2021-12-10 17:12:58 +1300

    Fixed Ball draw bug

--- BIT504_AS2_5029791/src/Ball.java
+++ BIT504_AS2_5029791/src/Ball.java
@@ -3,10 +3,11 @@ import java.awt.Color;
 public class Ball extends Sprite { // Extends to SuperClass
 
 	// Class Var
-	private int BALL_HEIGHT, BALL_WIDTH = 25;
+	private int BALL_HEIGHT = 25;
+	private int BALL_WIDTH = 25;
 	private Color BALL_COLOUR = Color.white;
 
-	public Ball(int panelWidth, int panelHeight) {
+	public Ball(int panelWidth, int panelHeight) { // Constructor
        setWidth(BALL_WIDTH);
        setHeight(BALL_HEIGHT);
        setColour(BALL_COLOUR);
